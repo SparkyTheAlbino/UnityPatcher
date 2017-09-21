4554fd6009091a9f1fbb9d16fe689b5c4fc907af
Harvey <harveythesparky@gmail.com>
2017-09-21 11:19:53 +0100

    Code cleanup

--- unity-patcher.py
+++ unity-patcher.py
@@ -1,35 +1,21 @@
 import binascii
 import time
 
-changes = 0
+u_path = 'Unity.exe' # Unity path, Local only #TODO Add in default/detect path
+op_path = 'Unity-Patched.exe' # Output binary #TODO Make this configurable
 
-u_path = 'Unity.exe'
-op_path = 'Unity-Patched.exe'
-
-orig_hex = "84C0750833C04883C4205BC38B034883C4205BC3"
-rep_hex = "84C0740833C04883C4205BC38B034883C4205BC3"
+orig_hex = "84C0750833C04883C4205BC38B034883C4205BC3" # Hex string in original binary
+rep_hex = "84C0740833C04883C4205BC38B034883C4205BC3"  # Hex string to replace with
 
 # Load binary to memory
 with open(u_path, "rb") as in_file:
-    in_bytes = binascii.hexlify(in_file.read()).decode('utf-8').upper()
-
-# Search for hex
-x = in_bytes.count(orig_hex)
-print(x)
+    in_hex = binascii.hexlify(in_file.read()).decode('utf-8').upper() # Read file in as Hex
 
 if x > 0:
-    # Write Patch
-    in_bytes = in_bytes.replace(orig_hex, rep_hex)
-    x = in_bytes.count(orig_hex)
-    print(x)
-
-    # Write back to file
-    in_bytes = binascii.unhexlify(in_bytes.encode('utf-8'))
-    with open(op_path, 'wb') as out_file:
-        out_file.write(in_bytes)
-
+    in_hex = in_hex.replace(orig_hex, rep_hex) # Apply Patch
+    in_hex = binascii.unhexlify(in_hex.encode('utf-8')) # Convert the Hex back into a byte objects
+    with open(op_path, 'wb') as out_file: # Write the bytes to the output binary
+        out_file.write(in_hex)
     print("Patching complete. Please replace your uniy.exe with the patched version")
-    time.sleep(3)
 else:
     print("Binary already patched")
-    time.sleep(3)
